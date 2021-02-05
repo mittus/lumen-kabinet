514c6d11c2b8907f918379950a9fcdbf9b94d2aa
Dmitry Kolesnikov <mittus@ya.ru>
2021-02-05 10:17:24 +0300

    add main menu

--- js/app.js
+++ js/app.js
@@ -6,4 +6,13 @@ $(function() {
             weekdays: ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'],
         },
     });
+
+
+})
+
+$(document).on('click', 'button[id=menu-btn]', function() {
+    var _this = $(this),
+        _attr = _this.attr('aria-expanded') == 'true' ? 'close' : 'menu';
+
+    _this.attr('uk-icon', _attr);
 })
